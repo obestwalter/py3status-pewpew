283fbfacff6f9374f0c70d666793479d6307f768
Olof Bjarnason <olof.bjarnason@gmail.com>
2019-07-13 17:58:59 +0200

    add failing test for path function

--- tests/test_pew3wm.py
+++ tests/test_pew3wm.py
@@ -1,2 +1,18 @@
+import logging
+
+from pew3wm import pew3wm as mut
+
+
+log = logging.getLogger(__name__)
+
+log.debug("testing")
+
 def test_sanity():
     assert True
+
+
+def test_path_finder():
+    log.debug("testing")
+    path = mut.find_pew_pew_path()
+    print(f'Path is {path}')
+    assert ['test'] == mut.find_pew_pew_path
