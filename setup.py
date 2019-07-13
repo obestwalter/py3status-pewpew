e7734ac297a824706eb840bc9951f98d38331120
Oliver Bestwalter <oliver.bestwalter@avira.com>
2019-07-13 17:19:38 +0200

    Fix project layout and install test deps

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="pew3wm",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
     install_requires=["evdev", "pyserial"],
+    package_dir={"": "src"},
     extras_require={"lint": ["pre-commit"], "test": ["pytest"]},
     entry_points={"console_scripts": ["pew3wm = pew3wm.pew3wm:cli"]},
 )
